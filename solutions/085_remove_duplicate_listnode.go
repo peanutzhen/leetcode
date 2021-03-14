963f63e35980d68ffbae918057314144be2bdfb4
astzls213 <astzls213@163.com>
2021-03-14 11:12:09 +0800

    删除排序链表的重复元素2

---
+++ solutions/085_remove_duplicate_listnode.go
@@ -0,0 +1,40 @@
+package main
+
+// ListNode for singly-linked list.
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+// 双指针
+func deleteDuplicates(head *ListNode) *ListNode {
+	if head == nil {
+		return nil
+	}
+	hair := &ListNode{0, head}
+	count := 1
+	end, slow, fast := hair, head, head.Next
+	for fast != nil {
+		if slow.Val == fast.Val {
+			fast = fast.Next
+			count++
+		} else if count > 1 {
+			end.Next = fast
+			slow = fast
+			fast = fast.Next
+			count = 1
+		} else {
+			end = slow
+			slow = slow.Next
+			fast = fast.Next
+		}
+	}
+	if count > 1 {
+		end.Next = fast
+	}
+	return hair.Next
+}
+
+func main() {
+	deleteDuplicates(&ListNode{1, &ListNode{1, nil}})
+}
