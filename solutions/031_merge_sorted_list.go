b90e19797ecaf6861959ddc30ce9fea88132277e
astzls213 <astzls213@163.com>
2021-03-18 09:40:24 +0800

    Add:031 O(1)空间复杂度解法

--- solutions/031_merge_sorted_list.go
+++ solutions/031_merge_sorted_list.go
@@ -7,30 +7,54 @@ type ListNode struct {
 }
 
 func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
-	header := &ListNode{0, nil}
-	tmp := header
-	for {
-		if l1 != nil && l2 != nil {
-			if l1.Val < l2.Val {
-				tmp.Next = &ListNode{l1.Val, nil}
-				tmp, l1 = tmp.Next, l1.Next
-			} else {
-				tmp.Next = &ListNode{l2.Val, nil}
-				tmp, l2 = tmp.Next, l2.Next
+	// O(n)空间复杂度
+	/*
+		header := &ListNode{0, nil}
+		tmp := header
+		for {
+			if l1 != nil && l2 != nil {
+				if l1.Val < l2.Val {
+					tmp.Next = &ListNode{l1.Val, nil}
+					tmp, l1 = tmp.Next, l1.Next
+				} else {
+					tmp.Next = &ListNode{l2.Val, nil}
+					tmp, l2 = tmp.Next, l2.Next
+				}
+				continue
+			} else if l1 == nil && l2 != nil {
+				for l2 != nil {
+					tmp.Next = &ListNode{l2.Val, nil}
+					tmp, l2 = tmp.Next, l2.Next
+				}
+			} else if l1 != nil && l2 == nil {
+				for l1 != nil {
+					tmp.Next = &ListNode{l1.Val, nil}
+					tmp, l1 = tmp.Next, l1.Next
+				}
 			}
-			continue
-		} else if l1 == nil && l2 != nil {
-			for l2 != nil {
-				tmp.Next = &ListNode{l2.Val, nil}
-				tmp, l2 = tmp.Next, l2.Next
-			}
-		} else if l1 != nil && l2 == nil {
-			for l1 != nil {
-				tmp.Next = &ListNode{l1.Val, nil}
-				tmp, l1 = tmp.Next, l1.Next
-			}
+			break
 		}
-		break
+		return header.Next
+	*/
+
+	// O(1)空间复杂度
+	p1, p2 := l1, l2
+	dummyHead := &ListNode{0, l1}
+	prev := dummyHead
+	for p1 != nil && p2 != nil {
+		if p1.Val < p2.Val {
+			prev.Next = p1
+			p1 = p1.Next
+		} else {
+			prev.Next = p2
+			p2 = p2.Next
+		}
+		prev = prev.Next
 	}
-	return header.Next
+	if p1 == nil {
+		prev.Next = p2
+	} else {
+		prev.Next = p1
+	}
+	return dummyHead.Next
 }
